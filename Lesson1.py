098c4f936d2d403ec25b9057d0286c41017398f1
FayzullaT <f_tulbaev@mail.ru>
2024-08-30 11:16:58 +0500

    Working with dictionary

--- Lesson1.py
+++ Lesson1.py
@@ -1,1 +1,10 @@
-print("1st lesson")
+# print("1st lesson")
+# car_0 = {'model':'Ferrari', 'rang':'qizil'}
+# print(car_0)
+# print(car_0['model'])
+# print(car_0['rang'])
+
+en_uz = {'apple':'olma', 'appricot':'urik', 'banana':'banan'}
+suz = en_uz.get('appl', 'Bunday suz yuq lug\'atda')
+print(suz)
+
